3ef2f98184dd84bc0ddefa156963db05a41574ef
Pierre Kohler <pete@localhost.localdomain>
2017-10-05 17:56:21 +0200

    suppression du cas "else"

--- bot.rb
+++ bot.rb
@@ -2,7 +2,7 @@
 require 'rubygems'
 require 'telegram/bot'
 
-token = 'token_here'
+token = 'my_token'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
@@ -13,8 +13,6 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_photo(chat_id: message.chat.id, photo: "https://i.pinimg.com/736x/5e/47/a3/5e47a3c6c1f85255c9e32f294a3dd173--doge-meme-portal.jpg")
     when '/addCalendar'
       bot.api.send_message(chat_id: message.chat.id, text: "TODO")
-    else
-      bot.api.send_message(chat_id: message.chat.id, text: "Je ne vous comprends pas ! :(")
     end
   end
 end
