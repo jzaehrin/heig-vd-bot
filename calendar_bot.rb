e100dd122e5cb2670ce0458fe241f6d11808d211
Pierre Kohler <pete@localhost.localdomain>
2017-10-12 16:24:27 +0200

    modifié :         calendar_bot.rb

--- calendar_bot.rb
+++ calendar_bot.rb
@@ -7,20 +7,22 @@ class CalendarBot < Bot
     end
 
     def listen(message)
-        case message.text
-        when '/!set_admin'
-            if @config["admin"] then
-                reponse(message, "The admin channel has already set !")
+        if message.respond_to?('text')
+            case message.text
+            when '/!set_admin'
+                if @config["admin"] then
+                    reponse(message, "The admin channel has already set !")
+                end
+                reponse(message, "This channel has been set to admin")
+            when '/!remove_admin'
+                reponse(message, "This channel has been removed")
+            when '/start_calendar'
+                reponse(message, "Hello, #{message.from.first_name}!")
+            when '/add_calendar'
+                reponse(message, "TODO")
+            when /^\/add_event ([A-Z]+)/
+                reponse(message, "Add event in #$1")
             end
-            reponse(message, "This channel has been set to admin")
-        when '/!remove_admin'
-            reponse(message, "This channel has been removed")
-        when '/start_calendar'
-            reponse(message, "Hello, #{message.from.first_name}!")
-        when '/add_calendar'
-            reponse(message, "TODO")
-        when /^\/add_event ([A-Z]+)/
-            reponse(message, "Add event in #$1")
         end
     end
 end
