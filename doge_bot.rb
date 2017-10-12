7fc21716cd0632e90adae3af0cbdc9dce9065275
Pierre Kohler <pete@localhost.localdomain>
2017-10-12 16:19:03 +0200

    modifié :         doge_bot.rb

--- doge_bot.rb
+++ doge_bot.rb
@@ -7,7 +7,7 @@ class DogeBot < Bot
     end
 
     def listen(message)
-        case message.text
+        case message
         when '/doge'
             bot.api.send_photo(chat_id: message.chat.id, photo: "https://i.pinimg.com/736x/5e/47/a3/5e47a3c6c1f85255c9e32f294a3dd173--doge-meme-portal.jpg")
         when Telegram::Bot::Types::CallbackQuery
