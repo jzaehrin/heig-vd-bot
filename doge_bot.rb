51c9138ff64cc566b736a8592e659143edfcf004
Pierre Kohler <pete@localhost.localdomain>
2017-10-12 16:05:14 +0200

    modifié :         doge_bot.rb

--- doge_bot.rb
+++ doge_bot.rb
@@ -10,7 +10,6 @@ class DogeBot < Bot
         case message.text
         when '/doge'
             bot.api.send_photo(chat_id: message.chat.id, photo: "https://i.pinimg.com/736x/5e/47/a3/5e47a3c6c1f85255c9e32f294a3dd173--doge-meme-portal.jpg")
-        end
         when Telegram::Bot::Types::Message
             kb = [
               Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Go to Google', url: 'https://google.com'),
