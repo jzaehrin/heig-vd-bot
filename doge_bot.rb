621c5b304f5135e2b7c28ada684c727912162ee5
Pierre Kohler <pete@localhost.localdomain>
2017-10-12 16:07:12 +0200

    modifié :         doge_bot.rb

--- doge_bot.rb
+++ doge_bot.rb
@@ -10,6 +10,11 @@ class DogeBot < Bot
         case message.text
         when '/doge'
             bot.api.send_photo(chat_id: message.chat.id, photo: "https://i.pinimg.com/736x/5e/47/a3/5e47a3c6c1f85255c9e32f294a3dd173--doge-meme-portal.jpg")
+        when Telegram::Bot::Types::CallbackQuery
+            # Here you can handle your callbacks from inline buttons
+            if message.data == 'touch'
+              bot.api.send_message(chat_id: message.from.id, text: "Don't touch me!")
+            end
         when Telegram::Bot::Types::Message
             kb = [
               Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Go to Google', url: 'https://google.com'),
