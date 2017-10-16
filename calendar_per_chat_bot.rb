3b27b5cc853821dbcdd34dfe51cb6871358290de
Pierre Kohler <pierrekohler94@gmail.com>
2017-10-16 11:21:41 +0200

    add files

---
+++ calendar_per_chat_bot.rb
@@ -0,0 +1,69 @@
+require './per_chat_bot'
+require './calendar'
+
+class CalendarPerChatBot < PerChatBot
+
+    def initialize(api)
+        super(api)
+    end
+
+    def new_worker(chat_id)
+        @workers = {chat_id => CalendarWorker.new(chat_id)}
+    end
+    
+    class CalendarWorker < PerChatBot::Worker
+
+        @@subject = ['INF1', 'ARO1', 'WTF3']
+        @@days_header = [['Mon','Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].zip([' ']*7)]
+
+        def initialize(chat_id)
+            super(chat_id)
+            @adding_event = Hash.new
+        end
+
+        def listen(message)
+            if @adding_event.empty?
+                if message.kind_of? Telegram::Bot::Types::CallbackQuery then listen_callback(message)
+                elsif message.respond_to?('chat') then listen_text(message) end
+            else
+                listen_adding_event(message)
+            end
+        end
+
+        def listen_text(message)
+            case message.text
+            when '/ok'
+                reponse("ko")
+            when '/add_event'
+                kbId = generate_ikb("Which class subject ?", @@subject.zip(@@subject).each_slice(3).to_a)
+                @adding_event = {kbId: kbId.to_s}
+            end
+        end
+
+        def listen_callback(message)
+            case message.data
+            when 'test'
+                reponse("ko")
+            end
+        end
+
+        def listen_adding_event(message)
+            if message.kind_of? Telegram::Bot::Types::CallbackQuery then
+                case message.data
+                when 'test'
+                    reponse("ko")
+                when /^([A-Z]+\d)/
+                    @adding_event.merge({subject: $1})
+                    delete_kb(@adding_event[:kbId])
+                    # use reponse
+                    reponse("/add_event #$1 SUMMARY", Telegram::Bot::Types::ForceReply.new(force_reply: true))
+                end
+            else
+                case message.text
+                when 'hello'
+                    nil
+                end
+            end
+        end
+    end
+end
---
+++ per_chat_bot.rb
@@ -0,0 +1,74 @@
+require 'abstraction'
+require './bot'
+
+class PerChatBot < Bot
+	abstract
+
+    def initialize(api)
+    	super(api)
+        @workers = Hash.new
+    end
+
+    def get_worker(chat_id)
+    	if @workers.empty? || !@workers.has_key?(chat_id)
+            new_worker(chat_id)
+        end
+        @workers[chat_id]
+    end
+
+    def new_worker(chat_id)
+        raise "override me as following:\n@workers.merge({chat_id => {WORKER_T_HERE}.new(chat_id)})"
+    end
+
+    def listen(message)
+    	chat_id = message.respond_to?('chat') ? message.chat.id : message.from.id
+        get_worker(chat_id).listen(message)   
+    end
+
+    class Worker < Bot
+    	abstract
+
+        def initialize(chat_id)
+            @chat_id = chat_id
+        end
+
+	    def reponse(text, reply_markup=nil)
+	        super(@chat_id.to_i, text, reply_markup)
+	    end
+
+        def delete_message(message_id)
+    		super(@chat_id, message_id)
+	    end
+
+	    def edit_markup(message_id, reply_markup=nil)
+	        super(@chat_id, message_id, reply_markup)
+	    end
+
+	    def delete_kb(message_id)
+	        super(@chat_id, message_id)
+	    end
+
+	    def response_photo(photo)
+	        super(@chat_id, photo)
+	    end
+
+	    def generate_ikb(text, buttons_infos)
+	        kb = buttons_infos.collect { |row|  
+	            row.collect { |button| Telegram::Bot::Types::InlineKeyboardButton.new(text: button.first, callback_data: button.last)
+	            }
+	        }
+	        markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+	        reponse(text, markup)['result']['message_id'] #returning only message_id from the new ikb
+	    end
+
+	    def edit_ikb(message_id, buttons_infos)
+	        kb = buttons_infos.collect { |row|  
+	            row.collect { |button| Telegram::Bot::Types::InlineKeyboardButton.new(text: button.first, callback_data: button.last)
+	            }
+	        }
+	        markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+	        edit_markup(message_id, markup)
+	    end
+
+    end
+end
