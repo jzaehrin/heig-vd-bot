c732aef98fca92b67f1a90a9ce4136fbf04550e7
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2017-10-17 12:54:03 +0200

    Add adminable module

--- admin.rb
+++ admin.rb
@@ -1,9 +1,31 @@
 module Adminable
     
-    def is_admin(user_id)
-        @config["admins"].include(user_id)
+    def admin?(chat_id)
+        @config["admins"].value?(chat_id)
     end
 
-    def add_admin
+    def id_admin?(id)
+        @config["admins"].key?(id)
+    end
+
+    def match_admin(chat_id, id, password)
+        if @config["invited_admin"].key?(id) && @config["invited_admin"][id] == password
+            @config["invited_admin"].delete(id)
+            @config["admin"][id] = chat_id
+        else
+         #return you aren't invited       
+        end
+    end
+
+    def add_admin(id, password)
+        @config["invited_admin"][id] = password
+    end
+
+    def remove_admin(id)
+        if id_admin?(id)
+            @config["admin"].delete(id)
+        else
+            #return this id isn't admin
+        end
     end
 end
